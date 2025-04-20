d228e6b7091f838f0a6d3072dad8e2dce588f80c
code-y02 <yatharthmain@gmail.com>
2025-04-20 22:21:48 +0530

    refactor

--- twitterplus-web/src/containers/Aside.tsx
+++ twitterplus-web/src/containers/Aside.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 const Aside = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="ring-1 ring-white/50 shadow-md shadow-white/50 rounded-2xl my-5 w-md sticky top-3 h-fit p-4 hidden lg:block ">
+    <div className="ring-1 ring-white/50 shadow-md shadow-white/50 rounded-2xl my-5 w-md sticky top-3 max-h-screen overflow-y-scroll p-4 hidden lg:block ">
       {children}
     </div>
   );
--- twitterplus-web/src/containers/Header.tsx
+++ twitterplus-web/src/containers/Header.tsx
@@ -1,11 +1,12 @@
 import SearchModal from "@/components/modals/SearchModal";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Input } from "@/components/ui/input";
 import { SearchIcon } from "lucide-react";
 
 function Header() {
   return (
     <div className="p-4 border-b-2 border-white/20  static top-0">
-      <div className="">
+      <div className="flex items-center justify-between">
         <SearchModal>
           <div className="bg-slate-800 inline-flex items-center px-3 py-1 md:w-md rounded-2xl">
             <Input
@@ -16,6 +17,13 @@ function Header() {
             <SearchIcon className="mr-3" />
           </div>
         </SearchModal>
+
+        <div className="">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>img</AvatarFallback>
+          </Avatar>
+        </div>
       </div>
     </div>
   );
